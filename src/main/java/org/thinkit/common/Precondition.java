f38d1b44116cbd553869c256879fd7afa78ab5b2
Kato Shinya <yourdream28@gmail.com>
2020-09-26 13:49:17 +0900

    add validate method

--- src/main/java/org/thinkit/common/Precondition.java
+++ src/main/java/org/thinkit/common/Precondition.java
@@ -197,6 +197,8 @@ public interface Precondition {
      * 引数として指定された {@code index} が {@code 0} から {@code to}
      * で指定された範囲内の数値か判定します。引数として指定された {@code index} が範囲外にある数値である場合は
      * {@link IndexOutOfBoundsException} が必ず実行時に発生します。
+     * <p>
+     * 任意の例外オブジェクトを指定する場合は {@link #requireRange(int, int, Throwable)} メソッドを使用してください。
      *
      * @param index 判定対象のインデックス
      * @param to    判定時の上限値
@@ -205,9 +207,34 @@ public interface Precondition {
      *                                   {@code to} で指定された範囲内に存在しない場合
      */
     static void requireRange(int index, int to) {
+        requireRange(index, to, new IndexOutOfBoundsException(
+                String.format("Index %s out-of-bounds for range from length 0 to length %s", index, to)));
+    }
+
+    /**
+     * 引数として指定された {@code index} が {@code 0} から {@code to}
+     * で指定された範囲内の数値か判定します。引数として指定された {@code index} が {@code 0} から {@code to}
+     * で指定された範囲内の数値ではない場合は、引数として指定された任意の例外オブジェクトをスローします。
+     * {@link #requireRange(int, int)} メソッドから実行された場合は
+     * {@link IndexOutOfBoundsException} を例外オブジェクトとしてスローします。
+     * <p>
+     * 任意の例外オブジェクトを指定しない場合は {@link #requireRange(int, int)} メソッドを使用してください。
+     *
+     * @param index     判定対象のインデックス
+     * @param to        判定時の上限値
+     * @param exception 前提条件を満たさなかった場合にスローされる任意の例外オブジェクト
+     *
+     * @exception NullPointerException      引数として渡された例外オブジェクトが {@code null} の場合
+     * @exception IndexOutOfBoundsException {@link #requireRange(int, int)}
+     *                                      メソッドから実行され、引数として指定された {@code number}
+     *                                      の数値が {@code 0} から {@code to}
+     *                                      で指定された範囲内に存在しない場合
+     */
+    static void requireRange(int index, int to, Throwable exception) {
+        requireNonNull(exception);
+
         if (index < 0 || to < index) {
-            throw new IndexOutOfBoundsException(
-                    String.format("Index %s out-of-bounds for range from length 0 to length %s", index, to));
+            error(exception);
         }
     }
 
@@ -215,6 +242,9 @@ public interface Precondition {
      * 引数として指定された {@code index} が {@code from} から {@code to}
      * で指定された範囲内の数値か判定します。引数として指定された {@code index} が範囲外にある数値である場合は
      * {@link IndexOutOfBoundsException} が必ず実行時に発生します。
+     * <p>
+     * 任意の例外オブジェクトを指定する場合は {@link #requireRange(int, int, int, Throwable)}
+     * メソッドを使用してください。
      *
      * @param index 判定対象のインデックス
      * @param from  判定時の最低値
@@ -224,9 +254,35 @@ public interface Precondition {
      *                                   から {@code to} で指定された範囲内に存在しない場合
      */
     static void requireRange(int index, int from, int to) {
+        requireRange(index, from, to, new IndexOutOfBoundsException(
+                String.format("Index %s out-of-bounds for range from length %s to length %s", index, from, to)));
+    }
+
+    /**
+     * 引数として指定された {@code index} が {@code from} から {@code to}
+     * で指定された範囲内の数値か判定します。引数として指定された {@code index}
+     * が範囲外にある数値である場合は引数として指定された任意の例外オブジェクトをスローします。
+     * {@link #requireRange(int, int, int)} メソッドから実行され、引数として指定された {@code index} が
+     * {@code from} から {@code to} で指定された範囲内の数値ではない場合は
+     * {@link IndexOutOfBoundsException} を例外オブジェクトとしてスローします。
+     * <p>
+     * 任意の例外オブジェクトを指定しない場合は {@link #requireRange(int, int, int)} メソッドを使用してください。
+     *
+     * @param index     判定対象のインデックス
+     * @param from      判定時の最低値
+     * @param to        判定時の上限値
+     * @param exception 前提条件を満たさなかった場合にスローされる任意の例外オブジェクト
+     *
+     * @exception NullPointerException      引数として渡された例外オブジェクトが {@code null} の場合
+     * @exception IndexOutOfBoundsException 引数として指定された {@code number} の数値が
+     *                                      {@code from} から {@code to}
+     *                                      で指定された範囲内に存在しない場合
+     */
+    static void requireRange(int index, int from, int to, Throwable exception) {
+        requireNonNull(exception);
+
         if (index < from || to < index) {
-            throw new IndexOutOfBoundsException(
-                    String.format("Index %s out-of-bounds for range from length %s to length %s", index, from, to));
+            error(exception);
         }
     }
 
